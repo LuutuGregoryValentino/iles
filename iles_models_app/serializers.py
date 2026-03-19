ab8a15e9001979a7dd8f1f808179329867f48b3f
mumberebreiline <breilinemumbere@gmail.com>
2026-03-19 16:55:21 +0300

    - Fix AssertionError by adding 'data=' to serializers in views.py - Configure project-level and app-level URL routing - Add basic GET and POST logic for workplace_supervisors - Clean up views.py imports and syntax

--- iles_models_app/serializers.py
+++ iles_models_app/serializers.py
@@ -1,24 +1,23 @@
-from .models import student,internship_administrator,workplace_supervisor,academic_supervisor,internship_placement,logbook_entry
-from rest_framework import  serializers
-class studentSrialiser(serializers.ModelSerializer):
+from .models import student,internship_administrator,workplace_supervisor, academic_supervisor, internship_placement,logbook_entry
+from rest_framework import serializers
+class studentSrializer(serializers.ModelSerializer):
        class Meta:
             model =student
             fields='__all__'
-class internship_administratorSrialiser(serializers.ModelSerializer):
+class internship_administratorSrializer(serializers.ModelSerializer):
         class Meta:
              model = internship_administrator
              fields='__all__'
-class workplace_supervisorSrialiser(serializers.ModelSerializer):
+class workplace_supervisorSrializer(serializers.ModelSerializer):
             class Meta:
-             modal =workplace_supervisor
+             model =workplace_supervisor
              fields='__all__'
-class internship_placementSrialiser(serializers.ModelSerializer):
+class internship_placementSrializer(serializers.ModelSerializer):
             class Meta:
-             modal =internship_placement
+             model =internship_placement
              fields='__all__'
 class logbook_entry(serializers.ModelSerializer):
             class Meta:
-             modal =logbook_entry
+             model =logbook_entry
              fields='__all__'
            
-    
--- iles_models_app/views.py
+++ iles_models_app/views.py
@@ -1,11 +1,30 @@
-from .models import student,internship_administrator,workplace_supervisor,academic_supervisor,internship_placement,logbook_entry
-from .serializers import  studentSrialiser,internship_administratorSrialiser,workplace_supervisorSrialiser,internship_placementSrialiser,logbook_entry
+from .models import student, workplace_supervisor, academic_supervisor, internship_placement, logbook_entry
+from .serializers import  studentSrializer,internship_administratorSrializer,workplace_supervisorSrializer, internship_placementSrializer, logbook_entry
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-@api_view(['GET'])
-def student_list(request):
-     Students =student.objects.all()
-     serializer =studentSrialiser(Students,many=True)
-     return Response(serializer.data)
 
+@api_view(['GET','POST'])
+def student_list(request):
+     if request.method== "GET":
+          Students =student.objects.all()
+          serializer =studentSrializer(Students,many=True)
+          return Response(serializer.data)
+     elif request.method == "POST":
+          serializer = studentSrializer(data = request.data)
+          if serializer.is_valid():
+               serializer .save()
+               return Response(serializer.data, status =201) 
+          return Response(serializer.errors, status =400)
+@api_view(['GET','POST'])
+def workplace_supervisors(request):
+     if request.method =="GET":
+          supervisor=workplace_supervisor.objects.all()
+          serializer =workplace_supervisorSrializer(supervisor, many=True)
+          return Response(serializer.data)
+     elif request.method == "POST":
+          serializer = workplace_supervisorSrializer(data= request.data)
+          if serializer.is_valid():
+               serializer.save
+               return Response(serializer.data,status =201)
+          return Response(serializer.errors,status=400)        
 # Create your views here.
