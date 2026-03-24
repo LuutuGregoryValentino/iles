d1b8e97eda918f7f3dc8077c5e02e991047a515c
stacykukunda <stacykukunda@gmail.com>
2026-03-24 23:52:59 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+import Signup from "./features/Signup/Signup"
+import Login from './features/Login/Login';
+
+function App() {
+
+  const [screen, setScreen] = useState("login"); //use of state and conditional rendering to swithch between the logina nd signup pages, 
+  return (
+    <div className='App'>
+      {screen === "login" && ( //if my state is login, load <Login /> set teh loginNavigate to swtich to signup upon click call
+        <Login loginNavigate={() => setScreen("signup")} /> 
+      )}
+
+      {screen === 'signup' && (
+        <Signup loginNavigate={() => setScreen('login')} />
+      )}
+    </div>
+  );
+}
+
+export default App;
