9d725da283379750417c6aa75c30672adc5fc72a
Raudha1301 <raur743@gmail.com>
2026-03-25 12:18:09 +0300

    mapped new api routes for supervisor and admin

--- iles_models_app/urls.py
+++ iles_models_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),           # homepage
-    path('student/', views.student_list, name='student_list'),  # optional
+    path('student/', views.student_list_api, name='student-list'),  # optional
+    path('supervisors/',views.supervisor_list_api,name='supervisor-list'),
+    path('admins/', views.admin_list_api,name='admin-list'),
 ]
