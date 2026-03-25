68835a778f481455b64efe67928aba0a738ba6fe
Raudha1301 <raur743@gmail.com>
2026-03-25 11:13:49 +0300

    Saving my local settings before pulling

--- my_project/settings.py
+++ my_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'iles_models_app'
+    'rest_framework',
+    'iles_models_app',
 ]
 
 MIDDLEWARE = [
