c55f6d1d37265610e3f86312638d60faa5e71297
Raudha1301 <raur743@gmail.com>
2026-04-06 17:23:33 +0300

    Finalized home dashboardwith studentlist navigation and fixedHTML structure

--- iles_models_app/views.py
+++ iles_models_app/views.py
@@ -3,6 +3,7 @@ from .serializers import  studentSerializer,internship_administratorSerializer,w
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import render
+
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -10,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 
 def home(request):
     return render(request, 'home.html')
+    
 
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
