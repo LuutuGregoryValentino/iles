cd5703875162a15124c0d43493ba762fffa91f66
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-12 12:04:27 +0300

    "Updated views.py in core to limit authentification to everyone"

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from django.shortcuts import redirect
+
 def login_view(request):
-    return render(request, 'login.html')
+    return redirect('http://localhost:3000')  # send everyone to React
 
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    return redirect('http://localhost:3000')
 
 def report_view(request):
     if request.method == 'POST':
