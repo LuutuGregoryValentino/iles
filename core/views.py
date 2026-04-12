5c8497934aee3de0673c385809fe372425e1e53c
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-12 15:20:19 +0300

    "Redirected  views.py in core to Apis.py"

--- core/views.py
+++ core/views.py
@@ -8,15 +8,13 @@ def login_view(request):
 def dashboard_view(request):
     return redirect('http://localhost:3000')
 
+from django.shortcuts import redirect
+
+def login_view(request):
+    return redirect('http://localhost:3000')
+
+def dashboard_view(request):
+    return redirect('http://localhost:3000')
+
 def report_view(request):
-    if request.method == 'POST':
-        date = request.POST.get('date')
-        description = request.POST.get('description')
-        hours = request.POST.get('hours')
-        challenges = request.POST.get('challenges')
-        attachment = request.FILES.get('attachment')
-
-
-        print(date, description, hours, challenges, attachment)  # temporary (we’ll save later)
-
-    return render(request, 'report.html')
+    return redirect('http://localhost:3000')
