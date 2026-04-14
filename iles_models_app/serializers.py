8605951c16bc4533551d7d061ba5bc7b76ddbd97
Luutu-Rahma <rahmaluutun@gmail.com>
2026-04-14 23:48:34 +0300

    "Updated ReadMe file and added new APi endpoints for the project, also updated the requirements.txt file"

--- iles_models_app/serializers.py
+++ iles_models_app/serializers.py
@@ -1,5 +1,24 @@
 from .models import student,internship_administrator,workplace_supervisor, academic_supervisor, internship_placement,logbook_entry,evaluation,issue
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'university_id', 'role', 'password']
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'university_id', 'role']
 class studentSerializer(serializers.ModelSerializer):
        class Meta:
             model =student
