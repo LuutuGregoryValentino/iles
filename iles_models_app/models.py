037535ee3bb5fab12f152828128a7e27018e977c
Luutu Rahma Namayanja <rahmaluutun@gmail.com>
2026-04-15 21:34:19 +0300

    Add validation on the hours worked in the logbook entry so they're realistic

--- iles_models_app/models.py
+++ iles_models_app/models.py
@@ -141,6 +141,16 @@ class LogbookEntry(models.Model):
 
     def __str__(self):
         return f"Week {self.week_number} — {self.placement.student.student_name}"
+    
+    def clean(self):
+        if self.hours_worked >120:
+            raise ValidationError({'hours_worked':"Are you a robot?Please enter realistic hours please(max 120 hours in a week)."})
+        if self.hours_worked <0:
+            raise ValidationError({'hours_worked':"Hours worked cannot be negative"})
+    def save(self,*args,**kwargs):
+        self.full_clean()
+        super().save(*args,**kwargs)
+        
 
 
 # ─── EVALUATION ──────────────────────────────────────────────────────────────
