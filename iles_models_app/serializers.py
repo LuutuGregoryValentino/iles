01aac9a332d9a32338bfa553d1f1a6ce01d85ed4
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-15 18:30:08 +0300

    "Removed spaces in the serializers.py file to fix the indentation error."

--- iles_models_app/serializers.py
+++ iles_models_app/serializers.py
@@ -14,7 +14,7 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
-      class Meta:
+    class Meta:
          model= User
          fields =[ 'id','email','university_id','role','password']
 
