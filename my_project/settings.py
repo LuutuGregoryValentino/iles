48b571a8bc19ae93d76416565251e11ee4389933
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-16 19:06:08 +0300

    " mODIFIED my_project/settings.py in the middleware"

--- my_project/settings.py
+++ my_project/settings.py
@@ -31,6 +31,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    # ← add this line
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -104,3 +105,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
