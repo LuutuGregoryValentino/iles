3ba7cba636bd1f17423fa800f6ec8f90883de5ad
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-17 00:19:44 +0300

    " Added yaml\.schema extensions in settings.json"

--- my_project/settings.py
+++ my_project/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-z@$_&)_fq^v9ck6n3nett7_a2nccc=b5q5m!yrdc-8sidi46eq')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-...')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
