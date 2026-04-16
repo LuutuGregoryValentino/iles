2e4e13ed55d2f0ecb267d7ebcd70e6188b5d7c16
stacykukunda <stacykukunda@gmail.com>
2026-04-16 22:46:41 +0300

    changes

--- iles_models_app/admin.py
+++ iles_models_app/admin.py
@@ -1,16 +1,30 @@
+
 from django.contrib import admin
-from .models import(User,Student,InternshipAdministrator,WorkplaceSupervisor,InternshipPlacement,LogbookEntry,Evaluation,Issue)
+from .models import *
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'course', 'year')
+    search_fields = ('user__username',)
+
+@admin.register(InternshipPlacement)
+class InternshipPlacementAdmin(admin.ModelAdmin):
+    list_display = ('student', 'company_name', 'start_date')
+
+@admin.register(LogbookEntry)
+class LogbookEntryAdmin(admin.ModelAdmin):
+    list_display = ('student', 'date', 'status')
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('student', 'title', 'status')
 
 admin.site.register(User)
-admin.site.register(Student)
 admin.site.register(InternshipAdministrator)
 admin.site.register(WorkplaceSupervisor)
-admin.site.register(InternshipPlacement)
-admin.site.register(LogbookEntry)
 admin.site.register(Evaluation)
-admin.site.register(Issue)
 admin.site.register(Project)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile) 
 
 
 
