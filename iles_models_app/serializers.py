3d8fd7fb908f75df8c27c85e9d0297d12071e487
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-17 18:26:36 +0300

    "Updated serializers.py by adding a create method to hash the password before saving the user instance."

--- iles_models_app/serializers.py
+++ iles_models_app/serializers.py
@@ -15,9 +15,16 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
-         model= User
-         fields =[ 'id','email','username','university_id','role','password']
+        model  = User
+        fields = ['id', 'email', 'username', 'university_id', 'role', 'password']
 
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user     = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model  = User
