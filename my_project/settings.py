51670fe021a1e9c99958f855afd31e4b4acedf9e
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-22 15:47:45 +0300

    " Updated the ALLOWED_HOSTS to fallback to 'iles-frontend.onrender.com' if the environment variable is not set"

--- my_project/settings.py
+++ my_project/settings.py
@@ -12,7 +12,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-z@$_&)_fq^v9ck6n3nett
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', 
+    'localhost,127.0.0.1', 'iles-frontend.onrender.com'
+).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
