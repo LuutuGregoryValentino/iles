086401b0e52656719440114dcb2523b1d5aa63cd
Luutu Rahma Namayanja <rahmaluutun@gmail.com>
2026-04-24 22:04:33 +0300

    Add email and settings imports to  views

--- iles_models_app/views.py
+++ iles_models_app/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.utils import timezone
+from django.core.mail import send_mail
+from django.conf import settings
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
