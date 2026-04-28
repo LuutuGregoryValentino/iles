458edaf94108cda5c51ec99c4aeffeb5a64ab703
OJAMBO NICHO <ojambonicholas052@gmail.com>
2026-04-28 23:10:34 +0300

    "Added email configuration to settings.py and removed redundant entries. "

--- my_project/settings.py
+++ my_project/settings.py
@@ -117,11 +117,11 @@ MEDIA_ROOT  = BASE_DIR / 'media'
 STATIC_ROOT         = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-EMAIL_BACKEND = 'django.core,mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
 
-"""EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'ILES Makerere <noreply@iles.mak.ac.ug>'"""
+DEFAULT_FROM_EMAIL = 'ILES Makerere <noreply@iles.mak.ac.ug>'
