803b2a2e39f49de6d1d8dc22a67c8d18cd574c5a
stacykukunda <stacykukunda@gmail.com>
2026-05-06 23:44:32 +0300

    removed merge conflict makers in settings.py

--- iles_models_app/apps.py
+++ iles_models_app/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class IlesModelsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'iles_models_app'
+    verbose_name = "ILES Internship Management System"
+
+    def ready(self):
+        """
+        This method runs when Django starts.
+        Use it to import signals or run startup logic.
+        """
+        import iles_models_app.signals  # connect signals
